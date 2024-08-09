5ce08ea599e32f0e9bf7821ee357ed98bb4ce6f6
juandeto <juandeto10@gmail.com>
2024-08-08 22:37:10 -0300

    styles

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -4,7 +4,8 @@ import { Outlet } from 'react-router-dom';
 export default function Layout() {
   return (
     <>
-      <header className="h-20 shadow-md p-6 flex justify-end">
+      <header className="h-20 shadow-md p-6 flex justify-between">
+        <p>Sync Tubes</p>
         <ModeToggle />
       </header>
       <Outlet />
--- client/src/screens/Landing.tsx
+++ client/src/screens/Landing.tsx
@@ -1,6 +1,7 @@
 import { Button } from 'components/Ui/Button';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from 'utils/routes';
+import { RefreshCcw, Share2, BadgeCheck, CirclePlay } from 'lucide-react';
 
 export default function Landing() {
   const navigate = useNavigate();
@@ -12,15 +13,29 @@ export default function Landing() {
   return (
     <div className="bg-background">
       <div className="flex justify-center items-center h-screen">
-        <div className="p-6 rounded-lg flex justify-center items-center flex-col w-1/2 mx-auto">
+        <div className="p-6 rounded-lg flex justify-center items-center flex-col lg:w-1/2 mx-auto">
           <p className="text-5xl">
-            <span className="px-4">{'{'}</span>
-            <span className="px-4">{'}'}</span>
+            <RefreshCcw className={'w-12 h-12 lg:w-24 lg:h-24'} />
           </p>
-          <h1 className="text-primary text-6xl m-8">
-            Out there in the woods...
+          <h1 className="text-primary text-3xl md:text-5xl lg:text-6xl m-8 text-center">
+            Sync Youtube videos with your friends
           </h1>{' '}
-          <Button size="lg" onClick={navigateToLogin} className="text-xl">
+          <ul className="flex flex-col justify-center items-center mb-6">
+            <li className="flex gap-2 m-3 md:text-xl">
+              <BadgeCheck /> Create channel
+            </li>
+            <li className="flex gap-2 m-3 md:text-xl">
+              <Share2 /> Share it with whoever you want
+            </li>
+            <li className="flex gap-2 m-3 md:text-xl">
+              <CirclePlay /> Enjoy sync youtube videos
+            </li>
+          </ul>
+          <Button
+            size="lg"
+            onClick={navigateToLogin}
+            className="text-md lg:text-xl"
+          >
             Start
           </Button>
         </div>
